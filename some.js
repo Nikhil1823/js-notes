90dc183ace51cc9a9007c14e71e61f42b0f497f1
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-07-16 10:37:12 +0530

    completed closures , scope , block ,hoisting etc and some array methods

--- some.js
+++ some.js
@@ -1,12 +1,15 @@
+var samuelName = "sad";
+function samuel() {
+  let sName = "happpy";
+  function danny() {
+    console.log(sName);
+    joman();
 
-let phones=[{name:'apple',cost:10},{name:'samsung',cost:10},{name:'vivo',cost:10}]
+    function joman() {
+      console.log(samuelName);
+    }
+  }
+  danny();
+}
 
-
-
-
-
-const test=phones.slice()
-test[0].cost=15
-
-console.log(phones);
-console.log(test);
+samuel();
