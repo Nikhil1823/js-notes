94ca90b49666613efc300a71df04d967c45c0f65
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-07-21 18:17:33 +0530

    completed one life cycle hook method

--- React/react-funda/src/WelcomeClass.jsx
+++ React/react-funda/src/WelcomeClass.jsx
@@ -4,19 +4,27 @@ import "./Styles/common.css";
 class Welcome extends Component {
   constructor(props) {
     super(props);
+    console.log("constructor of child component called");
     this.state = {
       email: "",
     };
   }
-
+  static getDerivedStateFromProps(props, state) {
+    state.email=props.name
+    console.log("getderived state from props");
+    return state
+  }
   handleSubmit(e) {
     console.log(this);
-    
+
     this.setState({ email: e.target.value });
     console.log(this.state.email);
   }
-
+  componentDidMount() {
+    console.log("compoemtn mounted from child");
+  }
   render() {
+    console.log("from render methods");
     return (
       <>
         <div className="input-field">
@@ -28,7 +36,9 @@ class Welcome extends Component {
             name=""
             id=""
             value={this.state.email}
-            onChange={function (e){this.handleSubmit(e)}}
+            onChange={(e) => {
+              this.handleSubmit(e);
+            }}
           />
           <p>{`Welcome user : ${this.state.email}`}</p>
         </div>
