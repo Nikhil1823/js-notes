60a742f72c02cc75fd1787202da8eeb0518183d7
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-07-25 19:54:50 +0530

    feat : completed react hooks

--- React/react-hook/src/components/custom Hooks/UserInputComp.jsx
+++ React/react-hook/src/components/custom Hooks/UserInputComp.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import useInput from "./hooks/useInput";
 const UserInputComp = () => {
   const [bindName, name, resetName] = useInput();
   const [bindAge, age, resetAge] = useInput();
-
+  const [fruits, setFruits] = useState([
+    "apple",
+    "orange",
+    "mango",
+    "bmw",
+    "audi",
+    "banaana",
+  ]);
+  const updateList = (i) => {
+    setFruits((prevFruit) => prevFruit.filter((_, index) => index != i));
+  };
   return (
     <div>
       <div className="input">
@@ -29,6 +39,12 @@ const UserInputComp = () => {
       >
         Clear
       </button>
+      <ul>
+        {console.log(fruits)}
+        {fruits.map((item, i) => {
+          return <li onClick={() => updateList(i)}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 };
