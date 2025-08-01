2956781cc537164d02a2813ee4bb74acefa8c6a4
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-08-01 14:35:50 +0530

    feat : header nav section is almost finished

--- airbnb/src/Components/Organisms/Header.tsx
+++ airbnb/src/Components/Organisms/Header.tsx
@@ -1,11 +1,12 @@
 // import React from 'react'
 import { clsx } from "clsx";
+import Navbar from "./Navbar";
 const Header = () => {
   return (
     <div className={clsx("header-bg w-full  h-[199.68px]  origin-top ")}>
-      <div className="nav h-[96px] inline-flex relative p-[48px]">
-        <p>hellos</p>
-      </div>
+      {/* <div className="nav h-[96px] inline-flex relative p-[48px]"> */}
+
+      <Navbar />
     </div>
   );
 };
