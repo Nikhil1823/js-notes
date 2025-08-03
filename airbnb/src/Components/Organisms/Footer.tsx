1cc2980b7bc9bec57a152b2db00f4dbcdabdb905
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-08-03 23:43:54 +0530

    feat : need to start doing the footer

--- airbnb/src/Components/Organisms/Footer.tsx
+++ airbnb/src/Components/Organisms/Footer.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 const Footer = () => {
-  return <div className="h-45 w-full mt-12.25"></div>;
+  return (
+    <div className="h-45 w-full mt-12.25">
+      <div className="top mx-12 py-12">
+        <div>contents here</div>
+      </div>
+
+      <div className="px-12">
+        <div>rest is here</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
