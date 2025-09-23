afd5f802c852f0af6e686d320fe66bcbb3636c4e
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-23 23:14:37 +0530

    Update 20250806080535-property_model_new.js

--- airbnb-backend/migrations/20250806080535-property_model_new.js
+++ airbnb-backend/migrations/20250806080535-property_model_new.js
@@ -1,15 +1,10 @@
 "use strict";
 const propertyModel = require("../models/propertyModel");
-
 module.exports = {
   async up(queryInterface, Sequelize) {
- 
-
     await queryInterface.createTable("property_model_new", propertyModel.model);
   },
-
   async down(queryInterface, Sequelize) {
- 
     await queryInterface.dropTable("property_model_new");
   },
 };
