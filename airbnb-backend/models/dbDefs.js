9c4688896dd17a79ccde6e080e462aab594876f6
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-23 23:13:13 +0530

    Update dbDefs.js

--- airbnb-backend/models/dbDefs.js
+++ airbnb-backend/models/dbDefs.js
@@ -3,17 +3,18 @@ const Sequelize = require("sequelize");
 const propertyModel = require("./propertyModel");
 const imageModel = require("./imagesModel");
 const db = new Sequelize(require("./config").dev);
+
 const propertyDbDef = db.define("propertyModel", propertyModel.model, {
   tableName: "propertyModel",
   timestamps: true,
 });
+
 const imagesDbDef = db.define("imagesModel", imageModel.model, {
   tableName: "imagesModel",
   timestamps: true,
 });
 
 db.authenticate();
-
 module.exports = {
   propertyDbDef,
   imagesDbDef,
