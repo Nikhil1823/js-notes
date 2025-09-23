74eac3bffae983faaf1a5a38666ac5ebda7ae026
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-23 23:13:45 +0530

    Update 20250806080535-property_model_new.js

--- airbnb-backend/migrations/20250806080535-property_model_new.js
+++ airbnb-backend/migrations/20250806080535-property_model_new.js
@@ -3,12 +3,7 @@ const propertyModel = require("../models/propertyModel");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+ 
 
     await queryInterface.createTable("property_model_new", propertyModel.model);
   },
