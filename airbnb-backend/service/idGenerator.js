35db7764460339ea17d2e7315a3395d43cfb749a
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-23 23:12:41 +0530

    Update idGenerator.js

--- airbnb-backend/service/idGenerator.js
+++ airbnb-backend/service/idGenerator.js
@@ -1,5 +1,5 @@
 const idGenerator = () => {
   return Date.now() + Math.floor(Math.random() * 1000);
 };
-
+//this will generate an id each tine we call this function
 module.exports = { idGenerator };
